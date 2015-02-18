80c67b6dbef2252072ca4cf127b30397957f7caf
Hayden Gascoigne <haydengascoigne@gmail.com>
2015-02-17 21:43:44 -0600

    Commit for 2/17

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -58,21 +58,4 @@ angular
   .constant('APIKEY', 'MGE0OWQ2ZjUwMDc4YjU0MzdkNTBiNzA4')
   .constant('APIPATH', 'http://st-studio.unl.edu/hyena_platform/public/api/1.0/')
   .constant('PLATFORM_ROOT', 'http://st-studio.unl.edu/hyena_platform/public/')
-  .constant('AUTH_SCOPE', 'groups')
-  .constant('angularMomentConfig', {
-    //timezone: 'America/Chicago'
-  })
-  .constant('AUTH_EVENTS', {
-    loginSuccess: 'auth-login-success',
-    loginFailed: 'auth-login-failed',
-    logoutSuccess: 'auth-logout-success',
-    sessionTimeout: 'auth-session-timeout',
-    notAuthenticated: 'auth-not-authenticated',
-    notAuthorized: 'auth-not-authorized'
-  })
-  .constant('USER_ROLES', {
-    all: '*',
-    admin: 'admin',
-    editor: 'editor',
-    guest: 'guest'
-  });
+  .constant('AUTH_SCOPE', 'groups');
